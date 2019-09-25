57eaaa3f6c207269002fc855b8b3ceff9149e1a3
Dr Kenneth Doig <ken.doig@petermac.org>
2019-09-25 14:25:42 +1000

    Update buildImage.sh
    make generic

--- Docker/build/buildImage.sh
+++ Docker/build/buildImage.sh
@@ -4,13 +4,13 @@ pathos_home=/usr/local/dev/PathOS/Curate
 cp $pathos_home/target/Curate-${version}.war PathOS.war
 cp $pathos_home/build/libs/Loader-all-${version}.jar Loader-all.jar
 
-docker login -u dockerpathos -p Scrooge0 docker.io
+docker login -u <user> -p <pass> docker.io
 
 docker build -t curate -f Dockerfile-curate .
 docker build -t loader -f Dockerfile-loader .
 
-docker tag curate:latest dockerpathos/pathos_curate:${version}
-docker tag loader:latest dockerpathos/pathos_loader:${version}
+docker tag curate:latest <user>/pathos_curate:${version}
+docker tag loader:latest <user>/pathos_loader:${version}
 
-docker push dockerpathos/pathos_curate:${version}
-docker push dockerpathos/pathos_loader:${version}
+docker push <user>/pathos_curate:${version}
+docker push <user>/pathos_loader:${version}
